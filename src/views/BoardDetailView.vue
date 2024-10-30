e1ffb79f86b67b846a7ede96ba924cc88b6edd8a
dbrnjstn <lkk030400@gmail.com>
2024-10-30 21:35:53 +0900

    이미지 추가 및 공지사항 페이지
    이미지 추가 및 공지사항 페이지

--- src/views/BoardDetailView.vue
+++ src/views/BoardDetailView.vue
@@ -6,10 +6,10 @@
     <!-- 본문 -->
     <div class="p-4 p-md-5 w-100">
 
-      <h1 class="mb-4 fs-2 fw-bold">{{notices[0].title}}</h1>
+      <h1 class="mb-4 fs-2 fw-bold">{{notice.noticeTitle}}</h1>
 
       <div class="d-flex justify-content-between align-items-center mb-0">
-        <p class="mb-0" style="color: #A2A2A2;">{{notices[0].date}}</p>
+        <p class="mb-0" style="color: #A2A2A2;">{{notice.noticeDate}}</p>
 
         <div class="dropdown">
           <button class="btn btn-link p-0" type="button" aria-expanded="false" id="detailBtn"  data-bs-toggle="dropdown">
@@ -39,7 +39,7 @@
       <hr>
 
       <div class="card p-4" style="height: 800px;">
-        {{notices[0].detail}}
+        {{notice.noticeContent}}
       </div>
 
 
@@ -56,8 +56,17 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import Header from "@/components/Header.vue";
-import router from '@/router';
+import { useRoute} from 'vue-router';
+import axios from 'axios';
 
+const route = useRoute(); // 현재 라우트의 정보
+
+const noticeId = route.params.noticeId;  // URL에서 noticeId 가져오기
+
+onMounted(() => {
+  console.log(noticeId);
+  getNoticeDetail();
+})
 
 // 수정 로직 구현
 function editPost() {
@@ -69,12 +78,35 @@ function deletePost() {
   console.log('게시물 삭제');
 }
 
-const notices = ref([
-  { id: 1, title: "게시글 1", date: "2024.10.11 18:52", detail:"이 글의 본문입니다."
-   }
-])
+const notice = ref({})
 
+const getNoticeDetail = async () => {
+  console.log('공지사항 요청 시작' );
 
+  try {
+    const response = await axios.post('/api/getNoticeDetail', { noticeId});
+    console.log('응답 데이터:', response);
+    // 공지사항 불러오기 성공 (상태 코드 200-299)
+    if (response.status >= 200 && response.status < 300) {
+      console.log(response.data + '공지사항 success');
+      notice.value = response.data;
+    } else {
+      // 예상치 못한 상태 코드
+      throw new Error('Unexpected response status');
+    }
+  } catch (error) {
+    if (error.response) {
+      // 서버가 2xx 범위를 벗어나는 상태 코드로 응답한 경우
+      console.error('공지사항 실패:', error.response.data.message || '알 수 없는 오류 발생');
+    } else if (error.request) {
+      // 요청이 전송되었으나 응답을 받지 못한 경우
+      console.error('서버 응답 없음');
+    } else {
+      // 요청 설정 중에 오류가 발생한 경우
+      console.error('요청 오류:', error.message);
+    }
+  }
+};
 </script>
 
 
