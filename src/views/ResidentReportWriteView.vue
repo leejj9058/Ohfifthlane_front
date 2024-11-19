bb23eb2efd4cd172f12b2be898227386f9e45f9a
yujin-kim <qkqh5682@naver.com>
2024-11-19 15:26:32 +0900

    장애인신고 완
    장애인신고 완

--- src/views/ResidentReportWriteView.vue
+++ src/views/ResidentReportWriteView.vue
@@ -101,7 +101,6 @@ const submitReport = async () => {
     report: {
       reportCarNumber: reportCarNumber.value,
       reportImage: photo.value ? photo.value.replace("http://172.168.10.73:8080/upload/", "") : "", // 사진 URL을 사용
-
       reportLat: currentLat.value, // 현재 위도
       reportLon: currentLon.value, // 현재 경도
     },
